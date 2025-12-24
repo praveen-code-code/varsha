87538dd88e4169c3260cd5e4cd76355e53864646
praveen star <loginsa80@gmail.com>
2025-12-24 12:38:20 +0530

    done

---
+++ function.js
@@ -0,0 +1,12 @@
+let a=30;
+let b=40;
+
+
+console.log(a+b);
+console.log(a&&b);
+
+let c=a<b || a===b;
+console.log(c);
+
+let d=a<b && a===b;
+console.log(d);
---
+++ script.js
@@ -0,0 +1,11 @@
+let age = 18;
+switch(age){
+    case 70:
+        console.log("old age")
+        break;
+    case 18:
+        console.log("young age")
+        break;
+    default :
+       console.log("not matched")
+}
